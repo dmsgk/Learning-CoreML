7be1910e9901756bc37cf7c0d278931a4d3381e6
johyeon yoon <yds03080@naver.com>
2021-11-20 22:24:13 +0900

    Tutorial1 initial commit

---
+++ CoreMLTutorial1/CoreMLTutorial1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMLTutorial1
+//
+//  Created by Johyeon Yoon on 2021/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
